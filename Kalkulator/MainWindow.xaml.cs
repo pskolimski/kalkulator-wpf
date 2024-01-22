98c5760dffce8cb897a716a2fc9244138593506d
Przemysław Skolimski <pskolimski@revku.dev>
2024-01-22 12:29:48 +0100

    Initial commit

---
+++ Kalkulator/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Kalkulator
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AcButton.Click += AcButtonOnClick;
+            PlusButton.Click += PlusButtonOnClick;
+            MinusButton.Click += MinusButtonOnClick;
+            EqualButton.Click += EqualButtonOnClick;
+            
+            ZeroButton.Click += NumberButtonOnClick;
+            OneButton.Click += NumberButtonOnClick;
+            TwoButton.Click += NumberButtonOnClick;
+            ThreeButton.Click += NumberButtonOnClick;
+            FourButton.Click += NumberButtonOnClick;
+            FiveButton.Click += NumberButtonOnClick;
+            SixButton.Click += NumberButtonOnClick;
+            SevenButton.Click += NumberButtonOnClick;
+            EightButton.Click += NumberButtonOnClick;
+            NineButton.Click += NumberButtonOnClick;
+        }
+
+        private void EqualButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MinusButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlusButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AcButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private void NumberButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                ResultLabel.Content += button.Content.ToString();
+            }
+        }
+    }
+}
